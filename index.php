681c2d4d17d33ebf3db8543eded60b14cbb771d4
pellowj <johnpellow1@student.kaplan.edu>
2017-10-22 18:30:17 -0500

    https://github.com/pellowj/Media-Library-inc-Database.git

--- index.php
+++ index.php
@@ -6,23 +6,23 @@ $pageTitle = "Personal Media Library";
 $section = null;
 
 include("inc/header.php"); ?>
-		<div class="section catalog random">
+    <div class="section catalog random">
 
-			<div class="wrapper">
+        <div class="wrapper">
 
-				<h2>May we suggest something?</h2>
+            <h2>May we suggest something?</h2>
 
-        <ul class="items">
+            <ul class="items">
             <?php
             $random = array_rand($catalog,4);
             foreach ($random as $id) {
                 echo get_item_html($id,$catalog[$id]);
             }
             ?>							
-				</ul>
+            </ul>
 
-			</div>
+        </div>
 
-		</div>
+    </div>
 
 <?php include("inc/footer.php"); ?>
