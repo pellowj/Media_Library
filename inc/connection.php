672009e8048f2e353bd629cd51d7652d930cc324
John Pellow <pellowj@aol.com>
2017-10-23 20:20:30 -0500

    cleaned up code some

--- inc/connection.php
+++ inc/connection.php
@@ -1,10 +1,10 @@
-<?php //new PHP Data Object 
-//This allows PHP to interact with a database 
-$dsn = "mysql:host=127.0.0.1;dbname=database;port=3306"; 
+<?php
+
+$dsn = "MySQL:host=127.0.0.1;dbname=database;port=3306"; 
 $username = "root"; 
 $password = "root"; 
-//(For local use, PHPMyAdmin doesn't use a password.) 
+
 $db = new PDO($dsn, $username, $password); 
-//if this dumps an object we are connected 
+ 
 var_dump($db); 
 ?>
