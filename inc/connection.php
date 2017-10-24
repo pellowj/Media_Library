4b217b313f869169b5aa2ae037a60987e95cd37f
John Pellow <pellowj@aol.com>
2017-10-23 20:13:14 -0500

    trying to connect php to mysql

--- inc/connection.php
+++ inc/connection.php
@@ -1,11 +1,10 @@
-<?php
-
-try {
-$db = new PDO("sqlite:".__DIR__."/database.db");
-vardump($db);
-} catch (Exception $e) {
-    echo"Unable to connect to database";
-    exit;
-}
-
-echo"Connection to the database";
+<?php //new PHP Data Object 
+//This allows PHP to interact with a database 
+$dsn = "mysql:host=127.0.0.1;dbname=database;port=3306"; 
+$username = "root"; 
+$password = "root"; 
+//(For local use, PHPMyAdmin doesn't use a password.) 
+$db = new PDO($dsn, $username, $password); 
+//if this dumps an object we are connected 
+var_dump($db); 
+?>
