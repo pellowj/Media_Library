06b4c66349c1614916eb3856bc7c88f9fef6e3e9
John Pellow <pellowj@aol.com>
2017-10-23 19:08:26 -0500

    try catch to make sure database is connected

--- inc/connection.php
+++ inc/connection.php
@@ -1,2 +1,11 @@
 <?php
+
+try {
 $db = new PDO("sqlite:".__DIR__."/database.db");
+vardump($db);
+} catch (Exception $e) {
+    echo"Unable to connect to database";
+    exit;
+}
+
+echo"Connection to the database";
