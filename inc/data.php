544768015bb3a70ac2b9669d08310c598903ed32
John Pellow <pellowj@aol.com>
2017-10-26 20:24:21 -0500

    included connection.php to data.php

--- inc/data.php
+++ inc/data.php
@@ -1,4 +1,6 @@
 <?php
+include("connection.php");
+/*
 $catalog = [];
 //Books
 $catalog[101] = [
@@ -183,4 +185,4 @@ $catalog[304] = [
     "year" => 2008,
     "category" => "Music",
     "artist" => "Nat King Cole"
-];
+];*/
